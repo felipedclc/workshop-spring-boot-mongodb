3da63ea1ae49eed8ec8b6bf0f561a1b8cc20b0c0
Felipe Cardoso <felipedclc@gmail.com>
2020-07-02 17:16:40 -0300

    Referenciando os posts do usuario

--- src/main/java/com/felipedclc/workshopmongo/config/Instantiation.java
+++ src/main/java/com/felipedclc/workshopmongo/config/Instantiation.java
@@ -42,5 +42,8 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null,sdf.parse("21/05/2019"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
--- src/main/java/com/felipedclc/workshopmongo/domain/User.java
+++ src/main/java/com/felipedclc/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.felipedclc.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document // LINKANDO COM O MONGODB
@@ -13,6 +16,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // REFERENCIA O POST NA CLASSE USER (POST SO SERA CARREGADO SE FOR ACESSADO) 
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -48,6 +54,14 @@ public class User implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
 
 	@Override
 	public int hashCode() {
@@ -73,5 +87,4 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
 }
