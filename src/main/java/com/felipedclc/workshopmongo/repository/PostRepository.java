b9ba0de3c9a5f4521e7ffc1fc4058900f22c2af8
Felipe Cardoso <felipedclc@gmail.com>
2020-07-03 14:01:25 -0300

    Consulta simples com @Querry

--- src/main/java/com/felipedclc/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/felipedclc/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.felipedclc.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.felipedclc.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") // ?0(PRIMEIRO PARAMETRO DO METODO) = text / i IGNORA O CASE SENSITIVE
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); 
 }
